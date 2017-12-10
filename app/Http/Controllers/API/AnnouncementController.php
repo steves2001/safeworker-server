dc68a97d4fd0cbfa76759424076df99d1b713f03
Stephen Smith <ssmith@lincolncollege.ac.uk>
2017-12-10 17:41:50 +0000

    specifying the which page to display implemented

--- app/Http/Controllers/API/AnnouncementController.php
+++ app/Http/Controllers/API/AnnouncementController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 use App\Http\Controllers\Controller;
 use Validator;
 use Illuminate\Support\Facades\DB;
@@ -30,12 +31,21 @@ class AnnouncementController extends Controller
 
         // Extract all the announcements for this source (pagination needs to be investigated)
         $search = $request->all();
+    
+        // Factor in the pagination if supplied
+        if(array_key_exists('page',$search)) {
+            $currentPage = $search['page'];
+            // Call the static method currentPageResolver() before querying
+            Paginator::currentPageResolver(function () use ($currentPage) {
+            return $currentPage;
+            });            
+        } 
         
         $announcements = DB::table('announcements')->where([
             ['source', '=', $search['source']],
             ['visible', '=', 'Y'],
-        ])->latest()->paginate(5);//->get();
-        
+        ])->latest()->paginate(3);
+
         // Nothing returned send an error (this might be better to just send an empty announcement)
         if ($announcements->count() == 0) {
            return response()->json(['error'=>['empty' => ['There are no new announcements for this category.']]], 401);
