c2bd34854c3cf5e7008d1e9d32f983f9c3526859
ssmith@lincolncollege.ac.uk <ssmith@lincolncollege.ac.uk>
2018-06-21 16:20:53 +0000

    seeding now has contact information

---
+++ resources/views/student/emergency.blade.php
@@ -0,0 +1,38 @@
+Safeguarding
+<ul><li>Safeguarding Team<br>safeguarding@lincolncollege.ac.uk<br>01522 86495 or 01522 864219<br>during office hours<br>&nbsp;</li><li>Out of hours duty emergency teams<br>01522 782333 (Lincolnshire)<br>03004 564546 (Nottinghamshire)<br>&nbsp;</li><li>Lincolnshire or Nottinghamshire Police<br>Telephone 101 to report a crime<br>Telephone 999 in an emergency</li></ul>
+Hate Crime
+<ul><li>Safeguarding Team<br>safeguarding@lincolncollege.ac.uk<br>01522 86495 or 01522 864219<br>during office hours<br>&nbsp;</li><li>Out of hours duty emergency teams<br>01522 782333 (Lincolnshire)<br>03004 564546 (Nottinghamshire)<br>&nbsp;</li><li>Lincolnshire or Nottinghamshire Police<br>Telephone 101 to report a crime<br>Telephone 999 in an emergency<br>&nbsp;</li><li><a href="https://www.stophateuk.org/">Stop Hate UK</a></li></ul>
+
+Bullying and Harassment
+<ul><li>Safeguarding Team<br>safeguarding@lincolncollege.ac.uk<br>01522 86495 or 01522 864219<br>during office hours<br>&nbsp;</li><li>Out of hours duty emergency teams<br>01522 782333 (Lincolnshire)<br>03004 564546 (Nottinghamshire)</li></ul>
+
+PREVENT
+<ul><li>Safeguarding Team<br>safeguarding@lincolncollege.ac.uk<br>01522 86495 or 01522 864219<br>during office hours<br>&nbsp;</li><li>Out of hours duty emergency teams<br>01522 782333 (Lincolnshire)<br>03004 564546 (Nottinghamshire)<br>&nbsp;</li><li>Lincolnshire or Nottinghamshire Police<br>Telephone 101 to report a crime<br>Telephone 999 in an emergency</li></ul>
+
+Mental Health
+<ul><li>Assessment Team<br>assessmentofficer@lincolncollege.ac.uk<br>01522 876225<br>during office hours<br>&nbsp;</li></ul>
+Assessment Team:
+E: assessmentofficer@lincolncollege.ac.uk 
+T: 01522 876225 during office hours.
+
+Counselling
+<ul><li>Student Services<br>studentservices<br>@lincolncollege.ac.uk<br>01522 876220<br>during office hours<br>&nbsp;</li></ul>
+ 
+
+Welfare
+<ul><li>Welfare Team<br>welfare@lincolncollege.ac.uk<br>01522 876220<br>during office hours<br>&nbsp;</li></ul>
+Welfare Team:
+E: welfare@lincolncollege.ac.uk 
+T: 01522 876220 during office hours
+
+Additional Learning Support
+<ul><li>Assessment Team<br>assessmentofficer<br>@lincolncollege.ac.uk<br>01522 876225<br>during office hours<br>&nbsp;</li></ul>
+Assessment Team:
+E: assessmentofficer@lincolncollege.ac.uk
+T: 01522 876225 during office hours
+
+Youth and Wellbeing
+<ul><li>Youth and Wellbeing Team<br>youthworker<br>@lincolncollege.ac.uk<br>01522 876220<br>during office hours<br>&nbsp;</li></ul>
+Youth and Wellbeing Team:
+E: youthworker@lincolncollege.ac.uk 
+T: 01522 876220 during office hours
